aa85a9c8f155072868fff93a49e2ebe00841cd06
XpliSyl <syl_element@hotmail.com>
2017-09-26 21:38:24 +0200

    testFinal

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    morgan  = require('morgan');
 
 app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.listen(server_port, server_ip_address, function(){
-  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+app.listen(port, ip, function(){
+  console.log('Server running on http://%s:%s', ip, port);
 });
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
 });
 
 module.exports = app ;
